881f578a98a6f154313b7f8dd73e48b8ff876fa8
fraro-coder <fr.aro@duocuc.cl>
2026-05-19 23:15:01 +0000

    Corrige API key Jenkins

--- build.sh
+++ build.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-cat > Dockerfile <<EOF
-FROM python:3.11
-
-WORKDIR /app
-
-COPY . .
-
-RUN pip install -r requirements.txt
-
-CMD ["python3", "app_clima.py"]
-EOF
-
 docker build -t climaapp .
+
+docker rm -f samplerunning || true
 
 docker run --name samplerunning \
 -e API_KEY_PROYECTO=$API_KEY_PROYECTO \
